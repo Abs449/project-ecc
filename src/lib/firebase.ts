7c3e8d08a424fb4f609af2bb4c0c08372c896b2a
Abs449 <cabhiram2506@gmail.com>
2026-05-22 01:17:34 +0530

    ..

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -34,7 +34,7 @@ let app: FirebaseApp | undefined;
 let auth: Auth | undefined;
 let db: Firestore | undefined;
 
-const isConfigValid = !!firebaseConfig.apiKey && firebaseConfig.apiKey !== 'AIzaSyCHBeEV24zkxXn1B9kL3bhtQhndU-1wGKQ';
+const isConfigValid = !!firebaseConfig.apiKey && firebaseConfig.apiKey !== 'your_api_key_here';
 
 if (getApps().length > 0) {
   try {
